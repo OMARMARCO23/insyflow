6f398293a0dd2cbb5fddd51a5329dc61cd1e7a21
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-09-18 23:01:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react';
 // IMPORTANT: change '/REPO_NAME/' to your repo name, e.g. '/insyflow/'
 export default defineConfig({
   plugins: [react()],
-  base: 'insyflow/', // <-- '/insyflow/' if your repo is named insyflow
+  base: '/insyflow/', // <-- '/insyflow/' if your repo is named insyflow
 });
-
