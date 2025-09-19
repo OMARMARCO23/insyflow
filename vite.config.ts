9115c159c65b4e899e4ad68abd4c3a200a4e5bac
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-09-19 22:56:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
+const isVercel = !!process.env.VERCEL;
+
 export default defineConfig({
   plugins: [react()],
-  base: '/insyflow/', // matches https://omarmarco23.github.io/insyflow/
+  base: isVercel ? '/' : '/insyflow/',
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
 });
